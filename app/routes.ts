903b8f3affb9473b3ff8903909def4d7b01fa8a4
dyns <ev117@gmail.com>
2025-07-09 14:37:24 -0600

    edit, delete tasks

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  index("routes/trip.tsx"),
+  // route("/trip", "routes/trip.tsx"),
   route("/category/:id", "routes/category.tsx"),
 ] satisfies RouteConfig;
--- app/types.ts
+++ app/types.ts
@@ -1,1 +1,7 @@
-export type Task = { name: string; complete: boolean };
+export type Task = { name: string; complete: boolean; id: string };
+
+export type Category = {
+  name: string;
+  id: string;
+  tasks: Task[];
+};
