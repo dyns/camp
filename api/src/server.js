92f2b1644ea8d6d192d2528cf47c17c5c1166d5c
dyns <ev117@gmail.com>
2025-07-16 22:09:47 -0600

    add docker-compose

--- api/src/server.js
+++ api/src/server.js
@@ -6,12 +6,13 @@ const fastify = Fastify({
 
 // Declare a route
 fastify.get("/", async function handler(request, reply) {
-  return { hello: "world" };
+  return { hello: "world update" };
 });
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 });
+  // await fastify.listen({ port: 3000 });
+  await fastify.listen({ port: 3000, host: "0.0.0.0" });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
