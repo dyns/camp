4686c606b37fce7176d29631f3f5d0aaecbdbaf9
dyns <ev117@gmail.com>
2025-07-17 21:47:05 -0600

    add file uploading for images

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,14 @@
 // Import the framework and instantiate it
 import Fastify from "fastify";
-
 import { PrismaClient } from "@prisma/client";
-import path from "node:path";
-
+import path from "path";
 import { fileURLToPath } from "url";
+
+// file upload imports
+import fs from "fs";
+import util from "util";
+import { pipeline } from "stream/promises";
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const srcDirname = path.dirname(__filename);
 const __dirname2 = path.dirname(srcDirname);
@@ -14,6 +18,8 @@ const prisma = new PrismaClient();
 const fastify = Fastify({
   logger: true,
 });
+
+fastify.register(import("@fastify/multipart"));
 
 // Declare a route
 fastify.get("/", async function handler(request, reply) {
@@ -32,6 +38,13 @@ console.log({ staticPath });
 fastify.register(import("@fastify/static"), {
   root: path.join(__dirname2, "uploads"),
   prefix: "/uploads/",
+});
+
+fastify.post("/users/profile-image", async function (req, reply) {
+  const data = await req.file();
+  const uploadPath = path.join(process.cwd(), "uploads", data.filename);
+  await pipeline(data.file, fs.createWriteStream(uploadPath));
+  return { url: `/uploads/${data.filename}` };
 });
 
 // Run the server!
