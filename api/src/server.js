aa72e20603bdb4cd7f82e527076b19541435089f
dyns <ev117@gmail.com>
2025-07-17 18:38:15 -0600

    add route to serve static files

--- api/src/server.js
+++ api/src/server.js
@@ -2,6 +2,12 @@
 import Fastify from "fastify";
 
 import { PrismaClient } from "@prisma/client";
+import path from "node:path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const srcDirname = path.dirname(__filename);
+const __dirname2 = path.dirname(srcDirname);
 
 const prisma = new PrismaClient();
 
@@ -18,6 +24,14 @@ fastify.get("/", async function handler(request, reply) {
 fastify.get("/users", async function handler(request, reply) {
   return await prisma.user.findMany();
   // return "these are the users";
+});
+
+const staticPath = path.join(__dirname2, "uploads");
+console.log({ staticPath });
+// serve static files
+fastify.register(import("@fastify/static"), {
+  root: path.join(__dirname2, "uploads"),
+  prefix: "/uploads/",
 });
 
 // Run the server!
