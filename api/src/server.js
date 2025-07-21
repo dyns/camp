a12aabce9bd2a4147d196b108a44d63f3065567c
dyns <ev117gm@gmail.com>
2025-07-21 00:12:03 -0600

    fix current session cookies

--- api/src/server.js
+++ api/src/server.js
@@ -3,25 +3,37 @@ import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 import path from "path";
 import { fileURLToPath } from "url";
+import fastifyCors from "@fastify/cors";
+import jwt from "jsonwebtoken";
 
 // file upload imports
 import fs from "fs";
-import util from "util";
 import { pipeline } from "stream/promises";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const srcDirname = path.dirname(__filename);
 const __dirname2 = path.dirname(srcDirname);
 
+const COOKIE_SECRET = "MY_SECRET";
+
 const prisma = new PrismaClient();
+
+const SESSION_COOKIE_NAME = "session";
+function createUserSessionCookie(userId) {
+  return jwt({ userId }, COOKIE_SECRET, { expiresIn: "1w" });
+}
 
 const fastify = Fastify({
   logger: true,
 });
 
+fastify.register(fastifyCors, {
+  origin: "http://localhost:5173", // Your React/Vite UI origin
+  credentials: true, // Allow cookies
+});
+
 fastify.register(import("@fastify/multipart"));
 
-fastify.register(import("@fastify/jwt"), { secret: "your-secret" });
 fastify.register(import("@fastify/cookie"));
 
 fastify.post("/auth/signup", async (request, reply) => {
@@ -43,7 +55,7 @@ fastify.post("/auth/signup", async (request, reply) => {
     });
   }
 
-  if (name.length < 3 || password.length < 6) {
+  if (name.length < 3 || password.length < 3) {
     return reply.status(400).send({
       error:
         "Name must be at least 3 characters and password at least 6 characters",
@@ -54,9 +66,10 @@ fastify.post("/auth/signup", async (request, reply) => {
     data: { name, password, email },
   });
 
-  const token = fastify.jwt.sign({ userId: user.id }, { expiresIn: "1d" });
+  // const token = fastify.jwt.sign({ userId: user.id }, { expiresIn: "1w" });
+  const session = createUserSessionCookie(user.id);
 
-  reply.setCookie("token", token, {
+  reply.setCookie("SESSION_COOKIE_NAME", session, {
     httpOnly: true,
     sameSite: "strict",
     path: "/",
@@ -65,31 +78,31 @@ fastify.post("/auth/signup", async (request, reply) => {
   return { success: true };
 });
 
-fastify.get("/check-auth", async (request, reply) => {
-  try {
-    const { userId } = await request.jwtVerify();
-    // request.user = { id: userId };
-    // Find user
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    });
-
-    return { authenticated: true, userId, user };
-  } catch {
-    return reply.status(401).send({ error: "Unauthorized" });
-  }
-});
-
 async function authMiddleware(request, reply) {
+  console.log("authMiddleware called", JSON.stringify(request.headers));
   try {
-    const { userId } = await request.jwtVerify();
-    // request.user = { id: userId };
-    // Find user
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    });
-    request.user = user;
-  } catch {
+    const sessionCookie = request.cookies[SESSION_COOKIE_NAME];
+    console.log("authMiddleware sessionCookie", sessionCookie);
+    if (!sessionCookie) {
+      return reply.status(401).send({ error: "Unauthorized" });
+    }
+
+    try {
+      const decoded = jwt.verify(sessionCookie, COOKIE_SECRET);
+      const userId = decoded.userId;
+      console.log("authMiddleware decoded", decoded);
+
+      // request.user = { id: userId };
+      // Find user
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+      request.user = user;
+    } catch {
+      return reply.status(401).send({ error: "Invalid token" });
+    }
+  } catch (error) {
+    console.error("Authentication error:", error);
     return reply.status(401).send({ error: "Unauthorized" });
   }
 }
@@ -109,11 +122,9 @@ fastify.post("/auth/signin", async (request, reply) => {
   // Validate password
   const isPasswordValid = password === user.password;
 
-  // Create JWT
-  const token = fastify.jwt.sign({ userId: user.id }, { expiresIn: "1d" });
+  const session = createUserSessionCookie(user.id);
 
-  // Set HTTP-only cookie
-  reply.setCookie("token", token, {
+  reply.setCookie("SESSION_COOKIE_NAME", session, {
     httpOnly: true,
     sameSite: "strict",
     path: "/",
@@ -152,8 +163,20 @@ fastify.post("/users/profile-image", async function (req, reply) {
 fastify.register(async function (protectedRoutes) {
   protectedRoutes.addHook("preHandler", authMiddleware);
 
-  protectedRoutes.get("/auth-middle", async (request, reply) => {
+  protectedRoutes.get("/auth/me", async (request, reply) => {
     return { userId: request.user.id, user: request.user };
+  });
+
+  protectedRoutes.get("/check-auth", async (request, reply) => {
+    try {
+      return {
+        authenticated: true,
+        userId: request.user.id,
+        user: request.user,
+      };
+    } catch {
+      return reply.status(401).send({ error: "Unauthorized" });
+    }
   });
 });
 
