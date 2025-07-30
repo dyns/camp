09220b7573180f784ea02dcd4fc9d24a9f7fa82e
dyns <ev117gm@gmail.com>
2025-07-29 19:25:41 -0600

    add patch task end point

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,9 @@ import { fastify } from "../lib/server.js";
 import tripsRoutes from "./trips.js";
 import usersRoutes from "./users.js";
 import authRoutes from "./auth.js";
+import tasksRoutes from "./tasks.js";
 
 fastify.register(usersRoutes, { prefix: "/users" });
 fastify.register(tripsRoutes, { prefix: "/trips" });
 fastify.register(authRoutes, { prefix: "/auth" });
+fastify.register(tasksRoutes, { prefix: "/tasks" });
---
+++ api/src/routes/tasks.js
@@ -0,0 +1,44 @@
+import { prisma } from "../lib/prisma.js";
+import { authMiddleware } from "../lib/middleware.js";
+
+export default async function routes(fastify) {
+  fastify.addHook("preHandler", authMiddleware);
+
+  fastify.post("/", async (request, resp) => {
+    const { name, categoryId } = request.body;
+
+    const createdTask = await prisma.task.create({
+      data: { name, categoryId },
+    });
+
+    return { task: createdTask };
+  });
+
+  fastify.patch(
+    "/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+          },
+          required: ["id"],
+        },
+      },
+    },
+    async (request, response) => {
+      const { id } = request.params;
+      const data = request.body || {};
+
+      console.log("task patch", id, typeof id, data);
+      const updated = await prisma.task.update({
+        where: { id },
+        data,
+        include: { category: { select: { tripId: true } } },
+      });
+
+      return updated;
+    }
+  );
+}
