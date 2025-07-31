0b13e00cc84cbe5afb9deb3639b7de72ac03963d
dyns <ev117gm@gmail.com>
2025-07-30 18:29:50 -0600

    add update category endpoint

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -10,4 +10,70 @@ export default async function routes(fastify) {
 
     return { category };
   });
+
+  fastify.get(
+    "/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+          },
+          required: ["id"],
+        },
+      },
+    },
+    async (request) => {
+      const { id } = request.params;
+
+      const category = await prisma.TripTaskCategory.findUnique({
+        where: { id },
+        include: {
+          trip: { select: { id: true, name: true } },
+        },
+      });
+
+      // Query completed and uncompleted tasks for this category
+      const completedTasks = await prisma.Task.findMany({
+        where: { categoryId: id, complete: true },
+      });
+      const uncompletedTasks = await prisma.Task.findMany({
+        where: { categoryId: id, complete: false },
+      });
+
+      // Attach to category object
+      category.completedTasks = completedTasks;
+      category.uncompletedTasks = uncompletedTasks;
+
+      return { category };
+    }
+  );
+
+  fastify.patch(
+    "/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+          },
+          required: ["id"],
+        },
+      },
+    },
+    async function updateCategory(request) {
+      const { id } = request.params;
+
+      const data = request.body || {};
+
+      const category = await prisma.TripTaskCategory.update({
+        where: { id },
+        data,
+      });
+
+      return { category: category };
+    }
+  );
 }
