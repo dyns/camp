9cf635fc4ee7ff48ef8fb6d6f4d1fb806dc4d27d
dyns <ev117gm@gmail.com>
2025-07-30 23:51:10 -0600

    add sign out route

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,8 +1,29 @@
 import { prisma } from "../lib/prisma.js";
+import { authMiddleware } from "../lib/middleware.js";
 
 export default async function routes(fastify) {
+  fastify.addHook("preHandler", authMiddleware);
+
   fastify.get("/", async function handler(request, reply) {
     return await prisma.user.findMany();
     // return "these are the users";
+  });
+
+  fastify.get("/me", async function getUser(request) {
+    const currentUser = request.user;
+    return { user: currentUser };
+  });
+
+  fastify.patch("/me", async function updateSessionUser(request) {
+    const currentUser = request.user;
+
+    const data = request.body || {};
+
+    const user = await prisma.user.update({
+      where: { id: currentUser.id },
+      data,
+    });
+
+    return { user };
   });
 }
