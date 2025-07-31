bea1951c4042d35e3d77034e36589a533f093556
dyns <ev117@gmail.com>
2025-07-30 18:51:04 -0600

    add functionality to create tasks from category details page

--- ui/app/apiClient/task.ts
+++ ui/app/apiClient/task.ts
@@ -19,7 +19,7 @@ export function useCreateTask(
       });
 
       queryClient.invalidateQueries({
-        queryKey: [`category:${task.category.id}`],
+        queryKey: [`category:${task.categoryId}`],
       });
     },
     onSettled: onSettled,
--- ui/app/trip/trip.tsx
+++ ui/app/trip/trip.tsx
@@ -5,76 +5,12 @@ import { Link, useParams } from "react-router";
 import { TaskItem } from "../components/task";
 import { Modal, useModal } from "../components/modal";
 import { EditTaskModal } from "../components/EditTaskModal";
+import { AddTaskModal } from "../components/AddTaskModal";
 
 import type { Task, Category } from "~/types";
 
 import { useGetTrip } from "../apiClient/trips";
 import { useUpdateTask, useCreateTask, useDeleteTask } from "../apiClient/task";
-
-function AddTaskModal({
-  addTaskToCategory,
-  modalRef,
-  showModal,
-  setShowModal,
-}: {
-  addTaskToCategory: (categoryId: number, taskText: string) => void;
-  modalRef: RefObject<HTMLDialogElement | null>;
-  showModal: { show: boolean; data: number | null };
-  setShowModal: Dispatch<
-    SetStateAction<{ show: boolean; data: number | null }>
-  >;
-}) {
-  const [todoText, setTodoText] = useState("test");
-
-  useEffect(() => {
-    if (showModal) {
-      setTodoText("");
-    }
-  }, [showModal]);
-
-  return (
-    <Modal ref={modalRef} dialogHTMLID="add-task-modal">
-      <h3 className="font-bold text-lg">Add a task</h3>
-      <p className="py-4">
-        <input
-          type="text"
-          placeholder="what else?"
-          className="input"
-          value={todoText}
-          onChange={(event) => {
-            setTodoText(event.target.value);
-          }}
-        />
-      </p>
-      <div className="modal-action">
-        <button
-          className="btn btn-outline btn-secondary"
-          onClick={() => {
-            setShowModal({ show: false, data: null });
-          }}
-        >
-          Cancel
-        </button>
-        <button
-          className="btn btn-soft btn-primary ml-4"
-          onClick={() => {
-            // add new todo
-            const trimmed = todoText.trim();
-            setTodoText(trimmed);
-
-            if (trimmed) {
-              const categoryId = Number.parseInt(showModal.data);
-              addTaskToCategory(categoryId, todoText);
-              setShowModal({ show: false, data: null });
-            }
-          }}
-        >
-          Add Task
-        </button>
-      </div>
-    </Modal>
-  );
-}
 
 export function TripPage() {
   const { id } = useParams();
